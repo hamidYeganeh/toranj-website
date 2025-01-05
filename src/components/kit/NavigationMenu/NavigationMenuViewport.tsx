3d7e0e7df94a672b4f8d3bb506d4756084b48ee8
Hamid_yeganeh_ <hamidyeganeh82@gmail.com>
2025-01-05 17:29:18 +0330

    [update] navigation menu ui fix

--- src/components/kit/NavigationMenu/NavigationMenuViewport.tsx
+++ src/components/kit/NavigationMenu/NavigationMenuViewport.tsx
@@ -7,7 +7,11 @@ export const NavigationMenuViewport = React.forwardRef<
     React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
     React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-    <div className={cn("absolute left-0 top-full flex justify-center")}>
+    <div
+        className={cn(
+            "absolute top-full flex justify-center ltr:start-0 rtl:end-0",
+        )}
+    >
         <NavigationMenuPrimitive.Viewport
             className={cn(
                 "origin-top-center bg-popover text-popover-foreground relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded border shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
